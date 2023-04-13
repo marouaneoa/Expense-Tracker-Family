dde36e552b3a6edf3985ae6f132c9af4e583b508
Marouane OULAD ALI <ouladalimarouane@gmail.com>
2023-04-13 12:59:18 +0100

    Initial Commit

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,44 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
+                <div class="sidebar-brand-text mx-3">{{ __('Homepage') }}</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            @can('user_management_access')
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <span>{{ __('User Management') }}</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item {{ request()->is('admin/permissions') || request()->is('admin/permissions/*') ? 'active' : '' }}" href="{{ route('admin.permissions.index') }}"> <i class="fa fa-briefcase mr-2"></i> {{ __('Permissions') }}</a>
+                        <a class="collapse-item {{ request()->is('admin/roles') || request()->is('admin/roles/*') ? 'active' : '' }}" href="{{ route('admin.roles.index') }}"><i class="fa fa-briefcase mr-2"></i> {{ __('Roles') }}</a>
+                        <a class="collapse-item {{ request()->is('admin/users') || request()->is('admin/users/*') ? 'active' : '' }}" href="{{ route('admin.users.index') }}"> <i class="fa fa-user mr-2"></i> {{ __('Users') }}</a>
+                    </div>
+                </div>
+            </li>
+            @endcan
+
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseExpense" aria-expanded="true" aria-controls="collapseTwo">
+                    <span>{{ __('Expense Management') }}</span>
+                </a>
+                <div id="collapseExpense" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item {{ request()->is('admin/expense_categories') || request()->is('admin/expense_categories/*') ? 'active' : '' }}" href="{{ route('admin.expense_categories.index') }}"> <i class="fa fa-briefcase mr-2"></i> {{ __('Expense category') }}</a>
+                        <a class="collapse-item {{ request()->is('admin/income_categories') || request()->is('admin/income_categories/*') ? 'active' : '' }}" href="{{ route('admin.income_categories.index') }}"> <i class="fa fa-briefcase mr-2"></i> {{ __('Income category') }}</a>
+                        <a class="collapse-item {{ request()->is('admin/expenses') || request()->is('admin/expenses/*') ? 'active' : '' }}" href="{{ route('admin.expenses.index') }}"> <i class="fa fa-briefcase mr-2"></i> {{ __('Expense') }}</a>
+                        <a class="collapse-item {{ request()->is('admin/incomes') || request()->is('admin/incomes/*') ? 'active' : '' }}" href="{{ route('admin.incomes.index') }}"> <i class="fa fa-briefcase mr-2"></i> {{ __('Income') }}</a>                    </div>
+                </div>
+            </li>
+
+
+        </ul>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('register');
+});
+
+Route::group(['middleware' => 'isAdmin','prefix' => 'admin', 'as' => 'admin.'], function() {
+    Route::get('dashboard', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard.index');
+    Route::resource('permissions', \App\Http\Controllers\Admin\PermissionController::class);
+    Route::resource('roles', \App\Http\Controllers\Admin\RoleController::class);
+    Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
+    Route::resource('expense_categories', \App\Http\Controllers\Admin\ExpenseCategoryController::class);
+    Route::resource('income_categories', \App\Http\Controllers\Admin\IncomeCategoryController::class);
+    Route::resource('expenses', \App\Http\Controllers\Admin\ExpenseController::class);
+    Route::resource('incomes', \App\Http\Controllers\Admin\IncomeController::class);
+});
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home');
